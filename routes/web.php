4c4aea277849bafc2803981df224ac4e18967ec7
fuadadhim24 <fuadadhim24@gmail.com>
2025-04-22 17:56:17 +0700

    feat: add fe quodiro promotion

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {return redirect('/admin');});
+Route::get('/', function () {return view('welcome');});
